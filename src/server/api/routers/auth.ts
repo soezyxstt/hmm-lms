9033e88ea239eeb41e3f5c3c36799e1b34ff4ec9
Adi Haditya <soezyxst@gmail.com>
2025-07-17 14:42:58 +0700

    rm createAdmin

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,11 +1,11 @@
 import { TRPCError } from "@trpc/server";
-import { z } from "zod";
+// import { z } from "zod";
 import { hashPassword } from "~/lib/utils";
 import { signUpSchema } from "~/lib/schema/auth";
 
 import {
   createTRPCRouter,
-  protectedProcedure,
+  // protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
 import { db } from "~/server/db";
@@ -51,68 +51,4 @@ export const authRouter = createTRPCRouter({
       userId: newUser.id,
     };
   }),
-  
-  createAdmin: protectedProcedure
-    .input(
-      z.object({
-        name: z.string().min(2, "Name must be at least 2 characters."),
-        email: z
-          .string()
-          .email("Invalid email address.")
-          .endsWith(
-            "@mahasiswa.itb.ac.id",
-            "Only ITB student emails are allowed.",
-          ),
-        password: z
-          .string()
-          .min(8, "Password must be at least 8 characters long."),
-      }),
-    )
-    .mutation(async ({ input, ctx }) => {
-      if (ctx.session?.user.role !== "ADMIN") {
-        throw new TRPCError({
-          code: "UNAUTHORIZED",
-          message: "You do not have permission to create an admin.",
-        });
-      }
-
-      const { name, email, password } = input;
-
-      // Check if email already exists
-      const existingUser = await db.user.findUnique({
-        where: { email: email.toLowerCase() },
-      });
-
-      if (existingUser) {
-        // Throw a specific tRPC error for existing email
-        throw new TRPCError({
-          code: "CONFLICT",
-          message: "Email already registered.",
-        });
-      }
-
-      const hashedPassword = await hashPassword(password);
-
-      const newAdmin = await db.user.create({
-        data: {
-          name,
-          email: email.toLowerCase(),
-          password: hashedPassword,
-          role: "ADMIN",
-        },
-      });
-
-      if (!newAdmin) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Failed to create admin. Please try again later.",
-        });
-      }
-
-      return {
-        success: true,
-        message: "Admin created successfully.",
-        userId: newAdmin.id,
-      };
-    }),
 });
